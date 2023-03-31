e889b06947d0972b2d7491461b4857bce729e4df
kadaradam <xadam1996@gmail.com>
2023-03-31 19:31:06 +0200

    feat: FE: add main route btn

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,5 +1,33 @@
-import { Outlet } from 'react-router-dom';
+import { useOutlet } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material';
 
-const Root = () => <Outlet />;
+const Root = () => {
+	const outlet = useOutlet();
+
+	return (
+		<>
+			{outlet ? (
+				outlet
+			) : (
+				<CenterBox>
+					<Button href="/drivers" variant="contained" color="primary">
+						Go to drivers
+					</Button>
+				</CenterBox>
+			)}
+		</>
+	);
+};
+
+export const CenterBox = styled(Box)({
+	height: '100vh',
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center',
+});
 
 export default Root;
