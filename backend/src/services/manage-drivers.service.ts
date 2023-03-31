6e880bd3975c4640505a0ca2c5d88caa05ce8081
kadaradam <xadam1996@gmail.com>
2023-03-31 12:40:10 +0200

    feat: add manage driver state service

---
+++ backend/src/services/manage-drivers.service.ts
@@ -0,0 +1,41 @@
+import { Driver } from '../interfaces';
+
+class ManageDriversService {
+	private drivers: Driver[];
+
+	constructor(driversInput: Driver[]) {
+		this.drivers = driversInput;
+	}
+
+	public getDrivers(): Driver[] {
+		return this.drivers;
+	}
+
+	public overtake(driverId: number): boolean {
+		const driverIndex = this.drivers.findIndex(
+			(racer) => racer.id === driverId,
+		);
+
+		if (driverIndex === -1) {
+			return false;
+		}
+
+		const currentPos = this.drivers[driverIndex].place;
+		const frontDriver = this.drivers.find(
+			(racer) => racer.place < currentPos,
+		);
+
+		if (!frontDriver) {
+			return false;
+		}
+
+		const frontDriverIndex = this.drivers.indexOf(frontDriver);
+
+		this.drivers[driverIndex].place = frontDriver.place;
+		this.drivers[frontDriverIndex].place = currentPos;
+
+		return true;
+	}
+}
+
+export { ManageDriversService };
