8eb36a2e6e43e3d916d3f056eeb078fd8852e24a
kadaradam <xadam1996@gmail.com>
2023-03-31 13:32:40 +0200

    feat: dark theming & roboto font

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -1,3 +1,20 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { Roboto } from '@next/font/google';
 
-export const theme = createTheme();
+export const roboto = Roboto({
+	weight: ['300', '400', '500', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+	fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+export const theme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+	typography: {
+		fontFamily: roboto.style.fontFamily,
+	},
+});
+
+export default responsiveFontSizes(theme);
