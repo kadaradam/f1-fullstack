56e514b7a9fc5891425719aefbca6294f33b670a
kadaradam <xadam1996@gmail.com>
2023-03-31 12:42:38 +0200

    fix: overtake http method to post

--- backend/src/modules/drivers/drivers.controller.ts
+++ backend/src/modules/drivers/drivers.controller.ts
@@ -6,7 +6,7 @@ export class DriversController implements Controller {
 	public router = Router();
 
 	constructor() {
-		this.router.get('/drivers/:driverId/overtake', this.overtake);
+		this.router.post('/drivers/:driverId/overtake', this.overtake);
 	}
 
 	private overtake(req: Request, res: Response) {
