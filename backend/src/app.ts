5a8263eedbf89ccdf8af3174987b0be60f3388ec
kadaradam <xadam1996@gmail.com>
2023-03-31 10:31:31 +0200

    feat: add formula1 data & serve images

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,14 +7,15 @@ dotenvConfig();
 import { config } from './config';
 import { join } from 'path';
 
-import { Test } from 'types';
-
 const app = express();
 const port = config.PORT;
 
 // Middlewares
 app.use(bodyParserJson());
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+
+// Server static assets
+app.use('/static', express.static(join(__dirname, '..', 'public', 'static')));
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(join(__dirname, '..', '..', 'frontend', 'dist')));
