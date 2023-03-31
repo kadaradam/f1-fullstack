c60773a44177ad007e952a225352886347a78e74
kadaradam <xadam1996@gmail.com>
2023-03-31 12:56:04 +0200

    feat: add drivers route

--- backend/src/modules/drivers/drivers.controller.ts
+++ backend/src/modules/drivers/drivers.controller.ts
@@ -6,7 +6,22 @@ export class DriversController implements Controller {
 	public router = Router();
 
 	constructor() {
+		this.router.get('/drivers', this.drivers);
 		this.router.post('/drivers/:driverId/overtake', this.overtake);
+	}
+
+	private drivers(req: Request, res: Response) {
+		try {
+			const drivers = driversState.getDrivers();
+
+			res.send({ success: true, data: drivers });
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({
+				success: false,
+				error: 'Unable to list drivers.',
+			});
+		}
 	}
 
 	private overtake(req: Request, res: Response) {
