f966e3cd9802ec1b3f6a4788f63aaafdc728b34d
kadaradam <xadam1996@gmail.com>
2023-04-04 21:42:26 +0200

    fix: overtake not incrementing place

--- backend/src/services/manage-drivers.service-prisma.ts
+++ backend/src/services/manage-drivers.service-prisma.ts
@@ -37,6 +37,10 @@ class ManageDriversServicePrisma {
 
 		const placeToMove = prismaDriverToMove.place - step;
 
+		if (placeToMove <= 0) {
+			return false;
+		}
+
 		const prismaFrontDriver = await prisma.driver.findFirst({
 			where: { place: placeToMove },
 		});
@@ -45,16 +49,45 @@ class ManageDriversServicePrisma {
 			return false;
 		}
 
-		await Promise.all([
-			prisma.driver.update({
+		if (step === 1) {
+			await Promise.all([
+				prisma.driver.update({
+					where: { id: prismaDriverToMove.id },
+					data: { place: prismaFrontDriver.place },
+				}),
+				prisma.driver.update({
+					where: { id: prismaFrontDriver.id },
+					data: { place: prismaDriverToMove.place },
+				}),
+			]);
+		} else if (step > 1) {
+			/* 
+				Must be asyncronous!!
+				Must update all "place" property before updating "prismaDriverToMove"
+			 */
+
+			await prisma.driver.updateMany({
+				where: {
+					AND: [
+						{
+							place: {
+								gte: prismaFrontDriver.place,
+							},
+						},
+						{
+							place: {
+								lt: prismaDriverToMove.place,
+							},
+						},
+					],
+				},
+				data: { place: { increment: 1 } },
+			});
+			await prisma.driver.update({
 				where: { id: prismaDriverToMove.id },
 				data: { place: prismaFrontDriver.place },
-			}),
-			prisma.driver.update({
-				where: { id: prismaFrontDriver.id },
-				data: { place: prismaDriverToMove.place },
-			}),
-		]);
+			});
+		}
 
 		return true;
 	}
