b9fb17e62c648d9486deae8fe7c4ec75979becb0
kadaradam <xadam1996@gmail.com>
2023-04-07 23:31:46 +0200

    feat: serve frontend on all express routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,22 +12,33 @@ import { flagImgProxyRouter } from './modules/flag-img-proxy/flag-img-proxy.rout
 
 const app = express();
 const port = config.PORT;
+const isProduction = process.env.NODE_ENV !== 'production';
 
 // Middlewares
 app.use(bodyParserJson());
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(morgan(isProduction ? 'combined' : 'dev'));
 
 // Server static assets
 app.use('/static', express.static(join(__dirname, '..', 'public', 'static')));
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(join(__dirname, '..', '..', 'frontend', 'dist')));
-}
 
 // Routes
 app.use(GLOBAL_API_PREFIX, [driversRouter]);
 
 // Image proxy
 app.use(flagImgProxyRouter);
+
+/* 
+	Serve the frontend on all routes.
+	Should be the last express route
+ */
+if (isProduction) {
+	app.use(express.static(join(__dirname, '..', '..', 'frontend', 'dist')));
+	app.get('*', (_, res) => {
+		res.sendFile(
+			join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'),
+		);
+	});
+}
 
 app.listen(port, () => {
 	console.log(`API is running on port ${port}. `);
